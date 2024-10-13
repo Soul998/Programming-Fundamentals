83c053cbf9d48dcc97e803f89647ed5ba6672a72
Soul998 <9108140@stud.nau.edu.ua>
2024-10-13 11:51:26 +0300

    Update main.cpp

--- labs/1.8/main.cpp
+++ labs/1.8/main.cpp
@@ -2,60 +2,58 @@
 Done by:
     Student: Yaroslav Yavon
     Group: 123:1
-     Lab 1.8
+    Lab 1.8
 */
-// 1. Перевірка парності числа
+
+// 1. Checking if a number is even
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
-        setlocale(LC_ALL, "RU");
 
     int num;
-    cout << "Введите целое число:";
+    cout << "Enter an integer: ";
     cin >> num;
-    bool iE = num % 2 == 0;
-    if (iE)
-        cout << "Число " << num << " парне\n" << endl;
+    bool isEven = num % 2 == 0;
+    if (isEven)
+        cout << "Number " << num << " is an even number\n" << endl;
     else
-        cout << "Число " << num << " непарне\n" << endl;
+        cout << "Number " << num << " is an odd number\n" << endl;
 
     return 0;
 }
 
-//2. Підрахунок кількості одиничних бітів
-
+// 2. Counting the number of 1-bits
 
 int main() {
-    setlocale(LC_ALL, "RU");
 
-    int ones = 0,
-        n;
-   cout << "Введите целое число:";
+    int ones = 0, n;
+    cout << "Enter an integer: ";
     cin >> n;
     for(int i = 0; i < sizeof(int) * 8; i++) {
         int mask = 1 << i;
         if((n & mask) != 0) ones++;
     }
-    cout << "Число " << n << " має " << ones << " 1 бит\n" << endl;
+    cout << "The number " << n << " has " << ones << " 1-bits\n" << endl;
     return 0;
 }
 
-//3. Перевірка встановленого біта на певній позиції
+// 3. Checking if a bit is set at a certain position
 
 int main() {
-    setlocale(LC_ALL, "RU");
 
-    int n,r;
-    cout << "Введите целое число: ";
+    int n, r;
+    cout << "Enter an integer: ";
     cin >> n;
-    cout << "Введите положение бита:";
+    cout << "Enter the bit position: ";
     cin >> r;
     if(r < 0) {
-        cout << "Бит постион до нуля\n";
+        cout << "Bit position is out of range\n";
         return 0;
     }
     int bitAtPos = (n & (1 << r)) == 0 ? 0 : 1;
-    cout << "Біт " << bitAtPos << " пастион " << r << "\n" << endl;
+    cout << "Bit at position " << r << " is " << bitAtPos << "\n" << endl;
+    return 0;
 }
+
